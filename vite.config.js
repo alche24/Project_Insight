62fd8976f4147bcb433875aaddf06bc60b25bd8c
brendanatashawp <brendanatashawp@gmail.com>
2026-03-23 15:15:58 +0700

    feat: add Google Finance API proxy configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,15 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
+      '/api/googlefinance': {
+        target: 'https://www.google.com',
+        changeOrigin: true,
+        headers: {
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'
+        },
+        rewrite: (path) => path.replace(/^\/api\/googlefinance/, '/finance/quote/COMPOSITE:IDX')
+      },
       '/api/yahoo': {
         target: 'https://query1.finance.yahoo.com',
         changeOrigin: true,
